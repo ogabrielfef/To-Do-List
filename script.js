72e5b0d342bc62e9483118340452fb643392c551
GabrielF <ogabrielfef@gmail.com>
2022-03-22 18:36:07 -0300

    requisitos 5-6

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+document.getElementById("criar-tarefa").addEventListener("click", function receberClick (){
+    let taskItens = document.getElementById("texto-tarefa").value;
+    let listTask = document.getElementById("lista-tarefas")
+    let createList = document.createElement("li")
+    createList.innerText = taskItens;
+    listTask.appendChild(createList);
+    document.getElementById('texto-tarefa').value ="";
+});
