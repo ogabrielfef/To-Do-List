3cb67c00e0288309829883d090aff0fdcc96eeb0
GabrielF <ogabrielfef@gmail.com>
2022-03-22 19:29:16 -0300

    requito 6

--- script.js
+++ script.js
@@ -4,7 +4,16 @@ document.getElementById("criar-tarefa").addEventListener("click", function receb
     let taskItens = document.getElementById("texto-tarefa").value;
     let listTask = document.getElementById("lista-tarefas")
     let createList = document.createElement("li")
+    createList.className = "itenList"
     createList.innerText = taskItens;
     listTask.appendChild(createList);
     document.getElementById('texto-tarefa').value ="";
+});
+
+//("itenList");
+document.getElementById("itenList").addEventListener("click", function trocaCor(){
+    let corDeFundo = document.getElementsByClassName("itenList").value;
+    corDeFundo.style.backgroundColor = 'gray'
+    corDeFundo.appendChild();
+    document.getElementsByClassName("itensList").value = "";
 });
