2b1438330fa04010301497d04ed52b51b08edb00
GabrielF <ogabrielfef@gmail.com>
2022-03-22 20:00:13 -0300

    erro reparado

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-document.getElementById("criar-tarefa").addEventListener("click", function receberClick (){
-    let taskItens = document.getElementById("texto-tarefa").value;
-    let listTask = document.getElementById("lista-tarefas")
-    let createList = document.createElement("li")
-    createList.className = "itenList"
+document.getElementById('criar-tarefa').addEventListener('click', function receberClick (){
+    let taskItens = document.getElementById('texto-tarefa').value;
+    let listTask = document.getElementById('lista-tarefas')
+    let createList = document.createElement('li')
+    createList.className = 'itenList'
     createList.innerText = taskItens;
     listTask.appendChild(createList);
-    document.getElementById('texto-tarefa').value ="";
+    document.getElementById('texto-tarefa').value ='';
 });
