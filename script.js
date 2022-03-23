5be9c4d04b5264c945d86adad1d831fa703c1de7
GabrielF <ogabrielfef@gmail.com>
2022-03-23 13:17:13 -0300

    enviando alguns requisitos

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.getElementById('criar-tarefa').addEventListener('click', function receberClick (){
+document.getElementById('criar-tarefa').addEventListener('click', function receberClick() {
     let taskItens = document.getElementById('texto-tarefa').value;
     let listTask = document.getElementById('lista-tarefas')
     let createList = document.createElement('li')
@@ -6,4 +6,17 @@ document.getElementById('criar-tarefa').addEventListener('click', function receb
     createList.innerText = taskItens;
     listTask.appendChild(createList);
     document.getElementById('texto-tarefa').value ='';
+    color(listTask);
 });
+
+function color (color){
+    color.addEventListener('click', function(event){
+       
+        let newColor = document.getElementsByClassName('ativo');
+
+        if(newColor.length !==0){
+            newColor[0].classList.remove('ativo');
+        }
+        event.target.classList.add('ativo');
+    })
+};
